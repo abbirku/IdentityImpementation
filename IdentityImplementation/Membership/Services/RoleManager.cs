94f2a5d00cfe4f7d82c43a95353927705006f64f
abbirku <abbir.ku@gmail.com>
2021-01-13 00:53:57 +0600

    Add RoleManager, SignInManager & UserManager

---
+++ IdentityImplementation/Membership/Services/RoleManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Demo.Membership.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.Membership.Services
+{
+    public class RoleManager
+        : RoleManager<ApplicationRole>
+    {
+        public RoleManager(IRoleStore<ApplicationRole> store, 
+            IEnumerable<IRoleValidator<ApplicationRole>> roleValidators, 
+            ILookupNormalizer keyNormalizer, 
+            IdentityErrorDescriber errors,
+            ILogger<RoleManager<ApplicationRole>> logger)
+            : base(store, roleValidators, keyNormalizer, errors, logger)
+        {
+        }
+    }
+}
