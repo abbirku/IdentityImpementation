a2d68e9eea0c055cfc4c02f49d003958400a135f
abbirku <abbir.ku@gmail.com>
2021-01-13 00:31:33 +0600

    Add ApplicationDbContext

---
+++ IdentityImplementation/Membership/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Demo.Membership.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Membership.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid,
+        ApplicationUserClaim, ApplicationUserRole, ApplicationUserLogin, ApplicationRoleClaim, ApplicationUserToken>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
